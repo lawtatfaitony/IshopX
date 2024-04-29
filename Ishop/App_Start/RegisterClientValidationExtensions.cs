f5d1c20bf4efe9fbd8da63e142a80dc10c1f8285
TonyLaw <lawtatfaitony@gmail.com>
2024-04-29 18:49:06 +0800

    up

---
+++ Ishop/App_Start/RegisterClientValidationExtensions.cs
@@ -0,0 +1,15 @@
+﻿using DataAnnotationsExtensions.ClientValidation;
+using DataAnnotationsExtensions.Web;
+
+[assembly: WebActivatorEx.PostApplicationStartMethod(typeof(AppStart_RegisterClientValidationExtensions), "Start")]
+
+namespace DataAnnotationsExtensions.Web
+{
+    public static class AppStart_RegisterClientValidationExtensions
+    {
+        public static void Start()
+        {
+            DataAnnotationsModelValidatorProviderExtensions.RegisterValidationExtensions();
+        }
+    }
+}
