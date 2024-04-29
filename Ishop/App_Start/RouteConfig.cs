4ff151947a692535c6196c55823eb35ea8288e5d
TonyLaw <lawtatfaitony@gmail.com>
2024-04-29 18:53:02 +0800

    up
    This reverts commit 3171c2fae22fdfb8163218f714ded28e813fdf74.

---
+++ Ishop/App_Start/RouteConfig.cs
@@ -0,0 +1,28 @@
+﻿using Ishop.Context;
+using LanguageResource;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Ishop
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes,string languageCode)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+             
+            routes.MapRoute(
+                        name: "Default",
+                        url: "{Language}/{controller}/{action}/{id}",
+                        constraints: new { Language = "zh-HK|zh-CN|en-US|hk|cn|en|HK|CN|EN" },
+                        defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional, Language = languageCode }   //   constraints: new { Language = "zh-HK|zh-CN|en-US" }, 
+                    );
+             
+        }
+
+    } 
+}
